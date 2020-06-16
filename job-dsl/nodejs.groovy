4c66218b0f33c000c53d49402c7583f6417648a0
Ashish Mathai <ashish@test.com>
2020-06-16 04:13:18 -0700

    update

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -14,10 +14,10 @@ job('NodeJS_dsl_test01') {
     }
     steps {
         shell("echo 'Hello, Welcome to DSL script Jenkins Job'")
-	shell('sshpass -p 'ashish' ssh -o StrictHostKeyChecking=no ashish@192.168.0.112 "date; date >> /tmp/date; sudo mkdir -p /opt/NodeProject/"')
-	shell('sshpass -p 'ashish' ssh -o StrictHostKeyChecking=no ashish@192.168.0.112 "cd /opt/NodeProject; git clone https://github.com/ashishmathai/nodejssample.git"')
-	shell('sshpass -p 'ashish' ssh -o StrictHostKeyChecking=no ashish@192.168.0.112 "cd /opt/NodeProject/nodejssample/; npm install"')
-	shell('sshpass -p 'ashish' ssh -o StrictHostKeyChecking=no ashish@192.168.0.112 "sudo chown -R ashish:ashish /opt/NodeProject; cd /opt/NodeProject/nodejssample; node index.js &"')
+	shell('sshpass -p "ashish" ssh -o StrictHostKeyChecking=no ashish@192.168.0.112 "date; date >> /tmp/date; sudo mkdir -p /opt/NodeProject/"')
+	shell('sshpass -p "ashish" ssh -o StrictHostKeyChecking=no ashish@192.168.0.112 "cd /opt/NodeProject; git clone https://github.com/ashishmathai/nodejssample.git"')
+	shell('sshpass -p "ashish" ssh -o StrictHostKeyChecking=no ashish@192.168.0.112 "cd /opt/NodeProject/nodejssample/; npm install"')
+	shell('sshpass -p "ashish" ssh -o StrictHostKeyChecking=no ashish@192.168.0.112 "sudo chown -R ashish:ashish /opt/NodeProject; cd /opt/NodeProject/nodejssample; node index.js &"')
 	shell('echo "Remote Curl Test"')
 	shell('curl 192.168.0.112:3000; curl -I 192.168.0.112:3000')
 	shell('echo "---------------------"')
