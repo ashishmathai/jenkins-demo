4e1ac5da9d68c36115f9f632c45f9ba8b2a201a0
Ashish Mathai <ashish@test.com>
2020-06-16 03:24:43 -0700

    update

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -14,8 +14,8 @@ job('NodeJS_dsl_test01') {
     }
     steps {
         shell("echo 'Hello, Welcome to DSL script Jenkins Job'")
-	shell(sshpass -p 'ashish' ssh -o StrictHostKeyChecking=no ashish@192.168.0.112 "date; date >> /tmp/date")
-	shell(sshpass -p 'ashish' ssh ashish@192.168.0.112 "sudo mkdir -p /opt/NodeProject/; sudo chown -R ashish:ashish /opt/NodeProject; cd /opt/NodeProject; git clone https://github.com/ashishmathai/nodejssample.git; cd /opt/NodeProject/nodejssample/; npm install; node index.js &")
-	shell(echo "---------------------"; echo 'Verify Node running index.js'; sshpass -p 'ashish' ssh -o StrictHostKeyChecking=no ashish@192.168.0.112 "sudo ps aux | grep -i index.js| grep -v grep";echo "---------------------"; echo "Local Curl Test" ;sshpass -p 'ashish' ssh -o StrictHostKeyChecking=no ashish@192.168.0.112 "curl localhost:3000; curl -I localhost:3000" ;echo "---------------------";echo "Remote Curl Test"; curl 192.168.0.112:3000; curl -I 192.168.0.112:3000; echo "---------------------")
+                command('echo "Hi"')                                                                                                                                                                                               command('sudo mkdir -p /opt/NodeProject/; sudo chown -R ashish:ashish /opt/NodeProject; cd /opt/NodeProject; git clone https://github.com/ashishmathai/nodejssample.git; cd /opt/NodeProject/nodejssample/; npm install; node index.js &"')
+                command('echo "---------------------"; echo 'Verify Node running index.js'; sshpass -p 'ashish' ssh -o StrictHostKeyChecking=no ashish@192.168.0.112 "sudo ps aux | grep -i index.js| grep -v grep";echo "---------------------"; echo "Local Curl Test" ;sshpass -p 'ashish' ssh -o StrictHostKeyChecking=no ashish@192.168.0.112 "curl localhost:3000; curl -I localhost:3000" ;echo "---------------------";echo "Remote Curl Test"; curl 192.168.0.112:3000; curl -I 192.168.0.112:3000; echo "---------------------"')
+        }
     }
 }
