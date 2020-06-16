7f86af431d86c02715204203d31b9b5efc913732
Ashish Mathai <ashish@test.com>
2020-06-16 04:28:20 -0700

    update

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -9,19 +9,29 @@ job('NodeJS_dsl_test01') {
         scm('* * * * *')
     }
     wrappers {
-        nodejs('nodejs') // this is the name of the NodeJS installation in 
+        nodejs('nodejs') // this is the name of the NodeJS installation in
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
         shell '''
-		"echo 'Hello, Welcome to DSL script Jenkins Job'
-		'sshpass -p "ashish" ssh -o StrictHostKeyChecking=no ashish@192.168.0.112 "date; date >> /tmp/date; sudo mkdir -p /opt/NodeProject/"
-		'sshpass -p "ashish" ssh -o StrictHostKeyChecking=no ashish@192.168.0.112 "cd /opt/NodeProject; git clone https://github.com/ashishmathai/nodejssample.git"
-		'sshpass -p "ashish" ssh -o StrictHostKeyChecking=no ashish@192.168.0.112 "cd /opt/NodeProject/nodejssample/; npm install"
-		'sshpass -p "ashish" ssh -o StrictHostKeyChecking=no ashish@192.168.0.112 "sudo chown -R ashish:ashish /opt/NodeProject; cd /opt/NodeProject/nodejssample; node index.js &"
-		'echo "Remote Curl Test"
-		'curl 192.168.0.112:3000; curl -I 192.168.0.112:3000
-		'echo "---------------------"
-	'''
+                echo "Hello, Welcome to DSL script Jenkins Job"
+
+		sshpass -p 'ashish' ssh -o StrictHostKeyChecking=no ashish@192.168.0.112 "ls /opt/NodeProject > /tmp/name"
+		if [[ sshpass -p 'ashish' ssh -o StrictHostKeyChecking=no ashish@192.168.0.112 "cat /tmp/name" != "nodejssample" ]]
+		then
+			echo "EXEC IF"
+			sshpass -p 'ashish' ssh -o StrictHostKeyChecking=no ashish@192.168.0.112 "cd /opt/NodeProject; git clone https://github.com/ashishmathai/nodejssample.git; cd /opt/NodeProject/nodejssample/; npm install; node index.js &"
+			shell (echo "---------------------"; echo 'Verify Node running index.js'; sshpass -p 'ashish' ssh -o StrictHostKeyChecking=no ashish@192.168.0.112 "sudo ps aux | grep -i index.js| grep -v grep";echo "---------------------"; echo "Local Curl Test" ;sshpass -p 'ashish' ssh -o StrictHostKeyChecking=no ashish@192.168.0.112 "curl localhost:3000; curl -I localhost:3000" ;echo "---------------------";echo "Remote Curl Test"; curl 192.168.0.112:3000; curl -I 192.168.0.112:3000; echo "---------------------")
+		else
+			echo "EXEC ELSE"
+			sshpass -p 'ashish' ssh -o StrictHostKeyChecking=no ashish@192.168.0.112 "cd /opt/NodeProject/nodejssample/; git pull https://github.com/ashishmathai/nodejssample.git; cd /opt/NodeProject/nodejssample/; npm install; node index.js &"
+			shell (echo "---------------------"; echo 'Verify Node running index.js'; sshpass -p 'ashish' ssh -o StrictHostKeyChecking=no ashish@192.168.0.112 "sudo ps aux | grep -i index.js| grep -v grep";echo "---------------------"; echo "Local Curl Test" ;sshpass -p 'ashish' ssh -o StrictHostKeyChecking=no ashish@192.168.0.112 "curl localhost:3000; curl -I localhost:3000" ;echo "---------------------")		 
+		fi
+		echo "Remote Curl Test"
+		curl 192.168.0.112:3000
+		curl -I 192.168.0.112:3000
+		echo "---------------------"
+
+        '''
     }
 }
