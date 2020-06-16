85a260a3cfa262350bb77f9cb06b7502eaf56a01
Ashish Mathai <ashish@test.com>
2020-06-16 00:32:41 -0700

    Update script nodejs dsl job

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,18 +1,18 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/ashishmathai/nodejssample.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('test@test.com')
         }
     }
     triggers {
-        scm('H/5 * * * *')
+        scm('* * * * *')
     }
     wrappers {
         nodejs('nodejs') // this is the name of the NodeJS installation in 
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
-        shell("npm install")
+        shell("echo 'Hello, Welcome to DSL script Jenkins Job'")
     }
 }
--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,12 +1,12 @@
-job('NodeJS Docker example') {
+job('NodeJS_Docker_example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/ashishmathai/nodejssample.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('test@test.com')
         }
     }
     triggers {
-        scm('H/5 * * * *')
+        scm('* * * * *')
     }
     wrappers {
         nodejs('nodejs') // this is the name of the NodeJS installation in 
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('ashishmathai/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
